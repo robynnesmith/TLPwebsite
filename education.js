648abb9540e88b5dd59802b07169ab292e4577d3
Robynne Smith <robynne.smith@centre4.local>
2017-11-01 16:25:51 +0000

    A level pictures and hover attempt

--- education.js
+++ education.js
@@ -3,21 +3,20 @@ function initMap() {
         ['Singapore', document.getElementById('sgmap'), 1.355223, 103.867787],
         ['Bath', document.getElementById('bathmap'), 51.378165, -2.327254]
     ];
-
     maps.forEach(function (map) {
         var coordinates = { lat: map[2], lng: map[3] };
-        var map = new google.maps.Map(map[1], {
+        var googleMap = new google.maps.Map(map[1], {
             zoom: 10,
             center: coordinates
         });
         var marker = new google.maps.Marker({
             position: coordinates,
-            map: map
+            map: googleMap
+        });
+        google.maps.event.addDomListener(window, "resize", function() {
+            var center = googleMap.getCenter();
+            google.maps.event.trigger(googleMap, "resize");
+            googleMap.setCenter(center); 
         });
     });
-
-    $(window).resize(function() {
-        google.maps.event.trigger(map, "resize");
-    });
 }
-
